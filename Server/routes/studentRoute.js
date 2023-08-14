8690a0e6a55e3bd39839fdc034ac0afebdba6f59
SamiRZL <rezalsami2001@gmail.com>
2023-08-14 19:00:17 +0100

    Adding student routes

--- Server/routes/studentRoute.js
+++ Server/routes/studentRoute.js
@@ -5,8 +5,55 @@ const mongoose = require('mongoose');
 
 router.post('/addStudent', async (req, res) => {
     try {
-        const user = await Student.create(req.body)
-        res.status(200).json({ message: 'User successfully added' })
+        await Student.create(req.body)
+        res.status(200).json({ message: 'Student successfully added' })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+router.put('/updateStudent/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const student = await Student.findByIdAndUpdate(id, req.body)
+        if (!student) {
+            res.status(404).json({ message: `Couldn't find student with the id of ${id}` })
+        }
+        const updatedStudent = await Student.findById(id)
+        res.status(200).json(updatedStudent)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+
+router.get('/students', async (req, res) => {
+    try {
+        const students = await Student.find({})
+        res.status(200).json(students)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+router.get('/students/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const student = await Course.findById(id)
+        res.status(200).json(student)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+router.delete('/deleteStudent/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const student = await Course.findByIdAndDelete(id)
+        if (!student) {
+            res.status(404).json({ message: `Couldn't find student with the id of ${id}` })
+        }
+        res.status(200).json({ message: 'Student deleted successfully' })
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
