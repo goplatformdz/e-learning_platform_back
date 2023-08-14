48c91b7c7b5e1ef72f1348b2523b37beb5da1af7
AmarBz22 <129527643+AmarBz22@users.noreply.github.com>
2023-08-14 14:09:20 +0100

    Update courseModel.js

--- Server/models/courseModel.js
+++ Server/models/courseModel.js
@@ -1,7 +1,24 @@
 const mongoose = require('mongoose');
 
 const courseSchema = mongoose.Schema({
-
-})
+  courseName: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  instructor: {
+    type: String,
+    required: true
+  },
+  enrolledStudents: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Student' 
+  }]
+});
 
 const Course = mongoose.model('Course', courseSchema);
+
+module.exports = Course;
