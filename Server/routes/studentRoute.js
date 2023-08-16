aab5403308386bd27e403a6a2eed56bd45a30398
AmarBz22 <129527643+AmarBz22@users.noreply.github.com>
2023-08-16 14:59:53 +0100

    Update studentRoute.js
    newsletter subscription router

--- Server/routes/studentRoute.js
+++ Server/routes/studentRoute.js
@@ -16,4 +16,18 @@ router.delete("/deleteStudent/:id", deleteStudent);
 router.get("/all-students", getAllStudents);
 router.get("/:id", getStudent);
 
+// New route for subscribing to the newsletter and sending confirmation email
+router.post('/subscribe-newsletter', async (req, res) => {
+    try {
+        const { email } = req.body;
+        await sendNewsletterConfirmationEmail(email);
+
+        // Respond to the client
+        res.status(200).json({ message: 'Newsletter subscription confirmed' });
+    } catch (error) {
+        console.error('Error subscribing to newsletter:', error);
+        res.status(500).json({ error: 'An error occurredd' });
+    }
+});
+
 module.exports = router;
